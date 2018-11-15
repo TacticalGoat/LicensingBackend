5073731ff41f3263594c5d4103742d23544b1ae9
Arjun Narayan <arjun@uninstall.io>
2018-11-15 22:08:00 +0530

    added post method and postgres support

--- app/views/license_check_view.py
+++ app/views/license_check_view.py
@@ -1,6 +1,7 @@
-from app import app
-from app import License
+from app import app, License, db
 from datetime import datetime
+from flask import jsonify, request
+from sqlalchemy import exc
 import json
 
 
@@ -17,5 +18,38 @@ def check_license(id):
         data["status"] = False
     else:
         data["status"] = True
-    return json.dumps(data), 200
+    return jsonify(data)
 
+
+@app.route("/license/machine", methods=["POST"])
+def create_license():
+    data = request.get_json()
+    required_params = ["machine_id", "start_time", "end_time"]
+    try:
+        assert all(param in data for param in required_params)
+        data["start_time"] = datetime.fromtimestamp(int(data["start_time"]))
+        data["end_time"] = datetime.fromtimestamp(int(data["end_time"]))
+        new_license = License(**data)
+        db.session.add(new_license)
+        db.session.commit()
+        data = {
+            "machine_id": new_license.machine_id,
+            "start_time": str(new_license.start_time),
+            "end_time": str(new_license.end_time)
+        }
+        return jsonify(data)
+    except AssertionError:
+        return jsonify(
+            {
+                "msg": "{0} are required parameters for this operation"
+                .format(", ".join(required_params))
+            }
+        ), 400
+    except exc.IntegrityError:
+        db.session.rollback()
+        return jsonify(
+            {
+                "msg": "Already Exists"
+                .format(", ".join(required_params))
+            }
+        ), 400
--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + str(os.path.join(basedir,
-                                            'license.db'))
+
+# SQLALCHEMY_DATABASE_URI = "sqlite:///" + str(os.path.join(basedir,
+#                                            'license.db'))
+
+SQLALCHEMY_DATABASE_URI = "postgres://db_user:DbUserPostgres@localhost:5432/\
+                           license_db"
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
